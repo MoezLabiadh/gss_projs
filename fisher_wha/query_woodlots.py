838d492e43a649310069a7548560af9ba9ced9ea
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2024-04-16 14:56:40 -0700

    Update query_woodlots.py

--- fisher_wha/query_woodlots.py
+++ fisher_wha/query_woodlots.py
@@ -4,6 +4,7 @@ warnings.simplefilter(action='ignore')
 import os
 import timeit
 import json
+import duckdb
 import cx_Oracle
 import pandas as pd
 import geopandas as gpd
@@ -45,6 +46,25 @@ class OracleConnector:
         if hasattr(self, 'connection') and self.connection:
             self.connection.close()
             print("....Disconnected from the database")
+
+
+class DuckDBConnector:
+    def __init__(self, db=':memory:'):
+        self.db = db
+        self.conn = None
+    
+    def connect_to_db(self):
+        """Connects to a DuckDB database and installs spatial extension."""
+        self.conn = duckdb.connect(self.db)
+        self.conn.install_extension('spatial')
+        self.conn.load_extension('spatial')
+        return self.conn
+    
+    def disconnect_db(self):
+        """Disconnects from the DuckDB database."""
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
             
 
 def read_query(connection,cursor,query,bvars):
@@ -56,16 +76,26 @@ def read_query(connection,cursor,query,bvars):
     
     return df
 
-            
-def create_aoi(in_shp):
-    """Return the extent of the draft polys """
-    gdf_all= gpd.read_file(in_shp)
-    extent = gdf_all.geometry.unary_union.envelope
-    gdf = gpd.GeoDataFrame(geometry=[extent], crs= gdf_all.crs)
+
+def esri_to_gdf (aoi):
+    """Returns a Geopandas file (gdf) based on 
+       an ESRI format vector (shp or featureclass/gdb)"""
+    
+    if '.shp' in aoi: 
+        gdf = gpd.read_file(aoi)
+    
+    elif '.gdb' in aoi:
+        l = aoi.split ('.gdb')
+        gdb = l[0] + '.gdb'
+        fc = os.path.basename(aoi)
+        gdf = gpd.read_file(filename= gdb, layer= fc)
+        
+    else:
+        raise Exception ('Format not recognized. Please provide a shp or featureclass (gdb)!')
     
     return gdf
 
-
+ 
 def get_wkb_srid(gdf):
     """Returns SRID and WKB objects from gdf"""
     srid = gdf.crs.to_epsg()
@@ -75,18 +105,23 @@ def get_wkb_srid(gdf):
         
     return wkb_aoi, srid
 
-def load_queries ():
-    sql = {}                                              
-    sql['wdlts'] = """
+
+def load_Orc_sql():
+    orSql= {}
+    
+    orSql['wdlts'] = """
         SELECT
             FOREST_FILE_ID,
             MAP_BLOCK_ID,
             ML_TYPE_CODE,
-            ROUND(SDO_GEOM.SDO_AREA(GEOMETRY, 0.5, 'unit=HECTARE'), 2) AS AREA_HA,
+            MAP_LABEL,
+            ROUND(SDO_GEOM.SDO_AREA(
+                GEOMETRY, 0.5, 'unit=HECTARE'), 2) AS WDLT_AREA_HA,
             LIFE_CYCLE_STATUS_CODE,
             CLIENT_NUMBER,
             CLIENT_NAME,
-            ADMIN_DISTRICT_CODE
+            ADMIN_DISTRICT_CODE,
+            SDO_UTIL.TO_WKTGEOMETRY(GEOMETRY) AS GEOMETRY
             
         FROM 
             WHSE_FOREST_TENURE.FTEN_MANAGED_LICENCE_POLY_SVW   
@@ -95,10 +130,198 @@ def load_queries ():
             FEATURE_CLASS_SKEY in ( 865, 866) 
             AND LIFE_CYCLE_STATUS_CODE <> 'RETIRED'
             AND SDO_WITHIN_DISTANCE (GEOMETRY, 
-                                     SDO_GEOMETRY(:wkb_aoi, :srid), 'distance=100 unit=m') = 'TRUE'
-                        """
+                                     SDO_GEOMETRY(:wkb_aoi, :srid), 'distance=500 unit=m') = 'TRUE'
+                    """
 
-    return sql
+    orSql['ofd'] = """
+        SELECT
+            CURRENT_PRIORITY_DEFERRAL_ID,
+            SDO_UTIL.TO_WKTGEOMETRY(SHAPE) AS GEOMETRY
+            
+        FROM
+            WHSE_FOREST_VEGETATION.OGSR_PRIORITY_DEF_AREA_CUR_SP ofd
+        
+        WHERE 
+            SDO_WITHIN_DISTANCE (SHAPE, 
+                        SDO_GEOMETRY(:wkb_aoi, :srid), 'distance=5000 unit=m') = 'TRUE'
+                    """
+            
+    return orSql
+
+
+def load_dck_sql():
+    dkSql= {}
+    dkSql['wdlts']="""
+        SELECT
+            FOREST_FILE_ID,
+            MAP_BLOCK_ID,
+            ML_TYPE_CODE,
+            MAP_LABEL,
+            ROUND(ST_Area(geometry) / 10000.0, 2) AS WDLT_AREA_HA,
+            LIFE_CYCLE_STATUS_CODE,
+            CLIENT_NUMBER,
+            CLIENT_NAME,
+            ADMIN_DISTRICT_CODE
+            
+        FROM 
+            wdlts  
+
+                    """
+                    
+    dkSql['wdlts_ofd']="""
+        SELECT
+            wdl.MAP_LABEL,
+            ROUND(ST_Area(wdl.geometry) / 10000.0, 2) AS WDLT_AREA_HA,
+            SUM(ROUND(ST_Area(
+                ST_Intersection(
+                    ofd.geometry, wdl.geometry)) / 10000.0, 2)) AS OFD_AREA_HA
+        
+        FROM 
+            wdlts wdl
+            LEFT JOIN ofd
+                ON ST_Intersects(ofd.geometry, wdl.geometry)
+                
+        GROUP BY 
+            wdl.MAP_LABEL,
+            ROUND(ST_Area(wdl.geometry) / 10000.0, 2)
+                    """
+
+    dkSql['wdlts_fhrw']="""
+        SELECT
+            wdl.MAP_LABEL,
+            ROUND(ST_Area(wdl.geometry) / 10000.0, 2) AS WDLT_AREA_HA,
+            SUM(ROUND(ST_Area(
+                ST_Intersection(
+                    fhrw.geometry, wdl.geometry)) / 10000.0, 2)) AS FHRW_AREA_HA
+        
+        FROM 
+            wdlts wdl
+            LEFT JOIN fisher_habitat_retention fhrw
+                ON ST_Intersects(wdl.geometry, fhrw.geometry)
+                
+        GROUP BY 
+            wdl.MAP_LABEL,
+            ROUND(ST_Area(wdl.geometry) / 10000.0, 2)
+                    """
+                    
+    return dkSql  
+
+
+def oracle_2_duckdb(orcCnx, orcCur, dckCnx, dict_sqls):
+    """Insert data from Oracle into a duckdb table"""
+    tables = {}
+    counter = 1
+    
+    for k, v in dict_sqls.items():
+        print(f'..adding table {counter} of {len(dict_sqls)}: {k}')
+        print('....export from Oracle')
+        if ':wkb_aoi' in v:
+            orcCur.setinputsizes(wkb_aoi=cx_Oracle.BLOB)
+            bvars = {'wkb_aoi':wkb_aoi,'srid':srid}
+            df = read_query(orcCnx, orcCur, v ,bvars)
+        else:
+            df = pd.read_sql(v, orcCnx)
+            
+
+        dck_tab_list= dckCnx.execute('SHOW TABLES').df()['name'].to_list()
+        
+        if k in dck_tab_list:
+            dck_row_count= dckCnx.execute(f'SELECT COUNT(*) FROM {k}').fetchone()[0]
+            dck_col_nams= dckCnx.execute(f"""SELECT column_name 
+                                             FROM INFORMATION_SCHEMA.COLUMNS 
+                                             WHERE table_name = '{k}'""").df()['column_name'].to_list()
+                
+            if (dck_row_count != len(df)) or (set(list(df.columns)) != set(dck_col_nams)):
+                print (f'....import to Duckdb ({df.shape[0]} rows)')
+                create_table_query = f"""
+                CREATE OR REPLACE TABLE {k} AS
+                  SELECT * EXCLUDE geometry, ST_GeomFromText(geometry) AS GEOMETRY
+                  FROM df;
+                """
+                dckCnx.execute(create_table_query)
+            else:
+                print('....data already in db: skip importing')
+                pass
+        
+        else:
+            print (f'....import to Duckdb ({df.shape[0]} rows)')
+            create_table_query = f"""
+            CREATE OR REPLACE TABLE {k} AS
+              SELECT * EXCLUDE geometry, ST_GeomFromText(geometry) AS GEOMETRY
+              FROM df;
+            """
+            dckCnx.execute(create_table_query)
+      
+        df = df.drop(columns=['GEOMETRY'])
+        
+        tables[k] = df
+      
+        counter += 1
+
+    return tables
+
+
+def gdf_to_duckdb (dckCnx, loc_dict):
+    """Insert data from a gdfs into a duckdb table """
+    tables = {}
+    counter= 1
+    for k, v in loc_dict.items():
+        print (f'..adding table {counter} of {len(loc_dict)}: {k}')
+        print ('....export from gdb')
+        df= esri_to_gdf (v)
+        df['GEOMETRY']= df['geometry'].apply(lambda x: wkb.dumps(x, output_dimension=2))
+        df = df.drop(columns=['geometry'])
+        
+        dck_tab_list= dckCnx.execute('SHOW TABLES').df()['name'].to_list()
+        
+        if k in dck_tab_list:
+            dck_row_count= dckCnx.execute(f'SELECT COUNT(*) FROM {k}').fetchone()[0]
+            dck_col_nams= dckCnx.execute(f"""SELECT column_name 
+                                             FROM INFORMATION_SCHEMA.COLUMNS 
+                                             WHERE table_name = '{k}'""").df()['column_name'].to_list()
+                
+            if (dck_row_count != len(df)) or (set(list(df.columns)) != set(dck_col_nams)):
+                print (f'....import to Duckdb ({df.shape[0]} rows)')
+                create_table_query = f"""
+                CREATE OR REPLACE TABLE {k} AS
+                  SELECT * EXCLUDE geometry, ST_GeomFromWKB(geometry) AS GEOMETRY
+                  FROM df;
+                """
+                dckCnx.execute(create_table_query)
+            else:
+                print('....data already in db: skip importing')
+                pass
+        
+        else:
+            print (f'....import to Duckdb ({df.shape[0]} rows)')
+            create_table_query = f"""
+            CREATE OR REPLACE TABLE {k} AS
+              SELECT * EXCLUDE geometry, ST_GeomFromWKB(geometry) AS GEOMETRY
+              FROM df;
+            """
+            dckCnx.execute(create_table_query)
+            
+        
+        df = df.drop(columns=['GEOMETRY'])
+        
+        tables[k] = df
+        
+        counter+= 1
+        
+    return tables
+
+def run_duckdb_queries (dckCnx, dict_sqls):
+    """Run duckdb queries """
+    results= {}
+    counter = 1
+    for k, v in dict_sqls.items():
+        counter = 1
+        print(f'..running query {counter} of {len(dict_sqls)}: {k}')
+        results[k]= dckCnx.execute(v).df()
+        
+        counter+= 1
+        
+    return results
 
 
 def generate_report (workspace, df_list, sheet_list,filename):
@@ -136,24 +359,41 @@ if __name__ == "__main__":
     
     wks= r'W:\srm\kam\Workarea\ksc_proj\Wildlife\Fisher\20240404_new_Fisher_draft_polygons'
     
-    print ('Connecting to BCGW.')
+    print ('Connect to databases')    
+    # Connect to the Oracle database
     Oracle = OracleConnector()
     Oracle.connect_to_db()
-    connection= Oracle.connection
-    cursor= Oracle.cursor
+    orcCnx= Oracle.connection
+    orcCur= Oracle.cursor
+    
+    # Connect to duckdb
+    Duckdb= DuckDBConnector(db='wdlt.db')
+    Duckdb.connect_to_db()
+    dckCnx= Duckdb.conn
     
     print ('Create an AOI shape.')
-    in_shp= os.path.join(wks, 'inputs', 'Draft_Fisher_Polygons_ALL.shp')
-    gdf= create_aoi(in_shp)
+    in_gdb= os.path.join(wks, 'inputs', 'data.gdb')
+    gdf_aoi= esri_to_gdf(os.path.join(in_gdb, 'Draft_Fisher_WHA_ALL_AOI'))
     #gdf.to_file(os.path.join(wks, 'tests', 'union.shp'))
-    wkb_aoi, srid= get_wkb_srid(gdf)
+    wkb_aoi, srid= get_wkb_srid(gdf_aoi)
     
     try:
-        print ('\nRun the Woodlot query.')
-        sql= load_queries ()
-        cursor.setinputsizes(wkb_aoi=cx_Oracle.BLOB)
-        bvars = {'wkb_aoi':wkb_aoi,'srid':srid}
-        df = read_query(connection,cursor,sql['wdlts'],bvars)
+
+        print ('\nLoad BCGW datasets')
+        orSql= load_Orc_sql ()
+        orcTables= oracle_2_duckdb(orcCnx, orcCur, dckCnx, orSql)
+        
+        print ('\nLoad local datasets')
+        gdb= os.path.join(wks,'test.gdb')
+        loc_dict={}
+        loc_dict['draft_fisher_polys']= os.path.join(in_gdb, 'Draft_Fisher_WHA_ALL')
+        loc_dict['fisher_habitat_retention']= os.path.join(in_gdb, 'fisher_habitat_retention')
+        gdbTables= gdf_to_duckdb (dckCnx, loc_dict)
+
+        
+        print('\nRun queries')
+        dksql= load_dck_sql()
+        rslts= run_duckdb_queries (dckCnx, dksql) 
         
 
     except Exception as e:
@@ -161,13 +401,14 @@ if __name__ == "__main__":
 
     finally: 
         Oracle.disconnect_db()
+        Duckdb.disconnect_db()
     
     
     print ('\nExport the report.')
     ouloc= os.path.join(wks, 'outputs')
     today = datetime.today().strftime('%Y%m%d')
-    filename= today + '_Fisher_polys_AOI_woodlots'
-    generate_report (ouloc, [df], ['woodlots'], filename)
+    filename= today + '_Fisher_draftPolys_woodlotsAnalysis'
+    generate_report (ouloc, rslts.values(), rslts.keys(), filename)
     
         
     finish_t = timeit.default_timer() #finish time
