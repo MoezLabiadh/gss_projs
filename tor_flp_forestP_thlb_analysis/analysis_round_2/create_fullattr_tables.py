d1f133251db70c9c4fc4c1a32d280ab937fa6880
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2024-11-08 10:39:09 -0800

    Create create_fullattr_tables.py

---
+++ tor_flp_forestP_thlb_analysis/analysis_round_2/create_fullattr_tables.py
@@ -0,0 +1,148 @@
+import warnings
+warnings.simplefilter(action='ignore')
+
+import os
+import timeit
+import duckdb
+import pandas as pd
+
+class DuckDBConnector:
+    def __init__(self, db=':memory:'):
+        self.db = db
+        self.conn = None
+    
+    def connect_to_db(self):
+        """Connects to a DuckDB database and installs spatial extension."""
+        self.conn = duckdb.connect(self.db)
+        self.conn.install_extension('spatial')
+        self.conn.load_extension('spatial')
+        return self.conn
+    
+    def disconnect_db(self):
+        """Disconnects from the DuckDB database."""
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+            
+
+def load_dck_sql():
+    dkSql= {} 
+    ######### Individual Criterea #################
+    '''
+    dkSql['r2_2_ogda_thlb_mdwr_fullattr']="""
+        CREATE TABLE r2_2_ogda_thlb_mdwr_fullattr AS
+        SELECT *
+        FROM r2_2_rip_idf_ogda_thlb_mdwr_fullattr
+        WHERE OVERLAP_TYPE IN ('OGDA only', 'IDF/OGDA overlap', 'Riparian/OGDA overlap', 'Riparian/IDF/OGDA overlap');
+
+        -- Build a spatial index
+        CREATE INDEX idx_r2_2_ogda_thlb_mdwr_fullattr ON r2_2_ogda_thlb_mdwr_fullattr USING RTREE (geometry);
+                 """   
+
+    dkSql['r2_2_idf_thlb_mdwr_fullattr']="""
+        CREATE TABLE r2_2_idf_thlb_mdwr_fullattr AS
+        SELECT *
+        FROM r2_2_rip_idf_ogda_thlb_mdwr_fullattr
+        WHERE OVERLAP_TYPE IN ('IDF only', 'IDF/OGDA overlap', 'Riparian/IDF overlap', 'Riparian/IDF/OGDA overlap');
+
+        -- Build a spatial index
+        CREATE INDEX idx_r2_2_idf_thlb_mdwr_fullattr ON r2_2_idf_thlb_mdwr_fullattr USING RTREE (geometry);
+                 """ 
+                 
+                 
+    dkSql['r2_2_rip_thlb_mdwr_fullattr']="""
+        CREATE TABLE r2_2_rip_thlb_mdwr_fullattr AS
+        SELECT *
+        FROM r2_2_rip_idf_ogda_thlb_mdwr_fullattr
+        WHERE OVERLAP_TYPE IN ('Riparian only', 'Riparian/IDF overlap', 'Riparian/OGDA overlap', 'Riparian/IDF/OGDA overlap');
+
+        -- Build a spatial index
+        CREATE INDEX idx_r2_2_rip_thlb_mdwr_fullattr ON r2_2_rip_thlb_mdwr_fullattr USING RTREE (geometry);
+                 """                  
+    '''
+    
+    
+    ######### Overlaps #################
+    dkSql['r2_2_rip_ogda_thlb_mdwr_fullattr']="""
+        CREATE TABLE r2_2_rip_ogda_thlb_mdwr_fullattr AS
+        SELECT *,
+            CASE 
+                WHEN OVERLAP_TYPE IN ('OGDA only', 'IDF/OGDA overlap') THEN 'OGDA only'
+                WHEN OVERLAP_TYPE IN ('Riparian only', 'Riparian/IDF overlap') THEN 'Riparian only'
+                WHEN OVERLAP_TYPE IN ('Riparian/OGDA overlap', 'Riparian/IDF/OGDA overlap') THEN 'Riparian/OGDA overlap'
+                ELSE NULL
+            END AS OVERLAP_TYPE_2
+        FROM r2_2_rip_idf_ogda_thlb_mdwr_fullattr
+        WHERE OVERLAP_TYPE IN ('OGDA only', 'IDF/OGDA overlap','Riparian only' ,'Riparian/IDF overlap' ,'Riparian/OGDA overlap', 'Riparian/IDF/OGDA overlap') ;
+
+        -- Build a spatial index
+        CREATE INDEX idx_r2_2_rip_ogda_thlb_mdwr_fullattr ON r2_2_rip_ogda_thlb_mdwr_fullattr USING RTREE (geometry);
+                 """     
+    
+
+    dkSql['r2_2_rip_idf_thlb_mdwr_fullattr']="""
+        CREATE TABLE r2_2_rip_idf_thlb_mdwr_fullattr AS
+        SELECT *,
+            CASE 
+                WHEN OVERLAP_TYPE IN ('IDF only', 'IDF/OGDA overlap') THEN 'IDF only'
+                WHEN OVERLAP_TYPE IN ('Riparian only', 'Riparian/OGDA overlap') THEN 'Riparian only'
+                WHEN OVERLAP_TYPE IN ('Riparian/IDF overlap', 'Riparian/IDF/OGDA overlap') THEN 'Riparian/IDF overlap'
+                ELSE NULL
+            END AS OVERLAP_TYPE_2
+        FROM r2_2_rip_idf_ogda_thlb_mdwr_fullattr
+        WHERE OVERLAP_TYPE IN ('IDF only', 'IDF/OGDA overlap','Riparian only' ,'Riparian/OGDA overlap' ,'Riparian/IDF overlap', 'Riparian/IDF/OGDA overlap')  ;
+
+        -- Build a spatial index
+        CREATE INDEX idx_r2_2_rip_idf_thlb_mdwr_fullattr ON r2_2_rip_idf_thlb_mdwr_fullattr USING RTREE (geometry);
+                 """ 
+                 
+    
+    return dkSql
+
+
+def run_duckdb_queries (dckCnx, dict_sqls):
+    """Run duckdb queries """
+    results= {}
+    counter = 1
+    for k, v in dict_sqls.items():
+        print(f'..running query {counter} of {len(dict_sqls)}: {k}')
+        results[k]= dckCnx.execute(v).df()
+        
+        counter+= 1
+        
+    return results
+
+    
+
+if __name__ == "__main__":
+    start_t = timeit.default_timer() #start time 
+    
+    wks= r'W:\lwbc\visr\Workarea\moez_labiadh\WORKSPACE_2024\20240819_flp_to_thlb_analysis'
+
+    print ('Connecting to databases')    
+    print ('..connect to Duckdb') 
+    projDB= os.path.join(wks, 'inputs', 'tor_flp_thlb_analysis.db')
+    Duckdb= DuckDBConnector(db= projDB)
+    Duckdb.connect_to_db()
+    dckCnx= Duckdb.conn 
+    dckCnx.execute("SET GLOBAL pandas_analyze_sample=1000000")
+    dckCnx.execute("PRAGMA max_temp_directory_size='100GiB'")
+    
+    try:
+        print ('Run Queries')
+        dksql= load_dck_sql()
+        run_duckdb_queries (dckCnx, dksql) 
+
+        
+    except Exception as e:
+        raise Exception(f"Error occurred: {e}")  
+    
+    finally: 
+        Duckdb.disconnect_db()
+        
+        
+finish_t = timeit.default_timer() #finish time
+t_sec = round(finish_t-start_t)
+mins = int (t_sec/60)
+secs = int (t_sec%60)
+print (f'\nProcessing Completed in {mins} minutes and {secs} seconds') 
